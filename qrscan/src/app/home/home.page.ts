2fb660de52f1167dfc648e83f41828743bd62684
Lakshyajit <lakshyajit165@gmail.com>
2020-09-20 23:19:15 +0530

    qr scanner poc complete - able to get details after scan

---
+++ qrscan/src/app/home/home.page.ts
@@ -0,0 +1,63 @@
+import { Component } from '@angular/core';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
+import { Platform } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  scanSub: any;
+
+  constructor(
+    public platform: Platform,
+    private qrScanner: QRScanner
+    ) {
+    this.platform.backButton.subscribeWithPriority(0, () => {
+      document.getElementsByTagName('body')[0].style.opacity = '1';
+      this.scanSub.unsubscribe();
+    });
+  }
+
+  encodedData = '';
+  QRSCANNED_DATA: string;
+  isOn = false;
+  scannedData: {};
+
+  scan() {
+    // Optionally request the permission early
+    this.qrScanner.prepare().
+      then((status: QRScannerStatus) => {
+        if (status.authorized) {
+          this.qrScanner.show();
+          this.scanSub = document.getElementsByTagName('body')[0].style.opacity = '0';
+          // debugger
+          this.scanSub = this.qrScanner.scan()
+            .subscribe((textFound: string) => {
+              document.getElementsByTagName('body')[0].style.opacity = '1';
+              this.qrScanner.hide();
+              this.scanSub.unsubscribe();
+
+              this.scannedData = textFound;
+              console.log(textFound);
+            }, (err) => {
+              alert(JSON.stringify(err));
+            });
+
+        } else if (status.denied) {
+        } else {
+
+        }
+      })
+      .catch((e: any) => console.log('Error is', e));
+  }
+
+  closeScanner() {
+    this.isOn = false;
+    this.qrScanner.hide();
+    this.qrScanner.destroy();
+  }
+
+}
